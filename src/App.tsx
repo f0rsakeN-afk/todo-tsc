cc56b0dc836153ba492e09a99b8a61c89f5de14b
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-03-09 17:23:29 +0545

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./components/model";
 
-const App = () => {
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos,setTodos]=useState<Todo[]>([])
+  //we can also use union <string|number> for variable data type
+  //console.log(todo);
+
+
   return (
-    <div>
-      Hello naresh
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
