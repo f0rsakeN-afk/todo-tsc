d3a41b87f48efe1506dc7a59104510294eaf1be7
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-03-11 20:39:51 +0545

    app done with tsx

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Todo } from "./model";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete, MdOutlineDone } from "react-icons/md";
@@ -28,25 +28,44 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  //edit todo
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  //for auto focus on edit
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className="todos__single">
-      <span
-        className={
-          todo.isDone ? "strike todos__single--text" : "todos__single--text"
-        }
-      >
-        {todo.todo}
-      </span>
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          type="text"
+          ref={inputRef}
+          value={editTodo}
+          className="input-field"
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : (
+        <span
+          className={
+            todo.isDone ? "strike todos__single--text" : "todos__single--text"
+          }
+        >
+          {todo.todo}
+        </span>
+      )}
 
       <div className="">
-        <span
-          className="icon"
-          onClick={() => {
-            if (!edit && todo.isDone) {
-              setEdit(!edit);
-            }
-          }}
-        >
+        <span className="icon" onClick={() => setEdit(!edit)}>
           <FaEdit />
         </span>
         <span className="icon" onClick={() => handleDelete(todo.id)}>
