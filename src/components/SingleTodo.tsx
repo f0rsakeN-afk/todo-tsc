9a958b15a3345266018320ff5507bcc86271765d
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-03-11 10:26:53 +0545

    xyz

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Todo } from "./model";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete, MdOutlineDone } from "react-icons/md";
+import "./styles.css";
 
 interface Props {
   todo: Todo;
@@ -7,9 +10,54 @@ interface Props {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-  return <form className="">
-    <span className="todos__single--text">{todo.todo}</span>
-  </form>
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  //mark todo as done
+  const handleDone = (id: number) => {
+    //console.log(id);
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  //delete todo
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <form className="todos__single">
+      <span
+        className={
+          todo.isDone ? "strike todos__single--text" : "todos__single--text"
+        }
+      >
+        {todo.todo}
+      </span>
+
+      <div className="">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <FaEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <MdDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdOutlineDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
